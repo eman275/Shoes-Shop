ae0e816c2e3249c86c969c02cae12e8191cc2090
eman275 <eman.altweel11@gmail.com>
2020-11-06 20:24:12 +0200

    some editing

--- app.js
+++ app.js
@@ -1,45 +1,3 @@
-
-const sizes = document.querySelectorAll('.size');
-const colors = document.querySelectorAll('.color');
-const shoes = document.querySelectorAll('.shoe ');
-const gradients = document.querySelectorAll('.gradient');
-// let prevColor = "blue";
-
-
-function changeSize(){
-    sizes.forEach(size => size.classList.remove('active'));
-    this.classList.add('active');
-}
-
-function changeColor(){
-     let primary = this.getAttribute('primary');
-     let color = this.getAttribute('color');
-     let shoe = document.querySelector(`.shoe[color="${color}"]`);
-    let gradient = document.querySelector(`.gradient[color="${color}"]`);
-    // let prevGradient = document.querySelector(`.gradient[color="${prevGradient}"]`);
-      
-    console.log(color);
-    console.log(primary);
-    document.documentElement.style.setProperty('--primary',primary);
-    colors.forEach( c => c.classList.remove('active'));
-    this.classList.add('active')
-    shoes.forEach (s => s.classList.remove('show'));
-    shoe.classList.add('show');
-    gradients.forEach(g => g.classList.remove('first', 'second'));
-    gradient.classList.add('first');
-    // prevGradient.classList.add('second');
-
-    // prevColor=color;
-    
-     
-    
-}
-
-
-sizes.forEach(size => size.addEventListener('click', changeSize));
- colors.forEach(c => c.addEventListener('click', changeColor));
-
-
 const sizes = document.querySelectorAll('.size');
 const colors = document.querySelectorAll('.color');
 const shoes = document.querySelectorAll('.shoe');
@@ -102,5 +60,3 @@ function changeHeight(){
 changeHeight();
 
 window.addEventListener('resize', changeHeight);
-
-document.addEventListener('contextmenu' , event => event.preventDefault());
